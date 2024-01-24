f5053c9fbd04b32557a8a31467a4abee8b2ad3d4
RAFAT CHOWDHURY <143308694+R4AT@users.noreply.github.com>
2024-01-24 10:16:03 +0600

    Update G-FILE.py

--- G-FILE.py
+++ G-FILE.py
@@ -1,5 +1,10 @@
-bit = Platform.architecture()[0]
-if =="64bit":
-  import FILE
-elif =="32bit":
-  import SEX
+import os, platform, time, sys
+try:
+ import requests
+except:os.system("pip uninstall requests -y;pip install requests")
+os.system('git pull --quiet 2>/dev/null')
+bit = platform.architecture()[0]
+if bit == '64bit':
+ import FILE 
+elif bit == '32bit':
+ import SEX 
